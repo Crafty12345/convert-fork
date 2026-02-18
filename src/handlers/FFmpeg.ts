0adebb441d02195da61458cd2a555a82ffa56a43
p2r3 <p2r3@p2r3.com>
2026-02-18 18:48:32 +0200

    remove faulty HEVC format from ffmpeg handler

--- src/handlers/FFmpeg.ts
+++ src/handlers/FFmpeg.ts
@@ -165,6 +165,8 @@ class FFmpegHandler implements FormatHandler {
 
     // AV1 doesn't seem to be included in WASM FFmpeg
     this.supportedFormats.splice(this.supportedFormats.findIndex(c => c.mime === "image/avif"), 1);
+    // HEVC stalls when attempted
+    this.supportedFormats.splice(this.supportedFormats.findIndex(c => c.mime === "video/hevc"), 1);
 
     // Add .qta (QuickTime Audio) support - uses same mov demuxer
     this.supportedFormats.push({
